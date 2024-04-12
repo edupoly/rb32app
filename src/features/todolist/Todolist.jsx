b575c5c23af18d8ebaf7eccb4a00143a134e15a6
edupoly <info.edupoly@gmail.com>
2024-04-12 07:24:02 +0530

    xcc

--- src/features/todolist/Todolist.jsx
+++ src/features/todolist/Todolist.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import Todo from './Todo'
 import { useEffect } from 'react';
 function Todolist() {
+    
     var [todos,setTodos] = React.useState(['Pay rent','get car','play football'])
     var [newTodo,setNewTodo] = React.useState('');
+    var nref = React.useRef();
     function updateNewTodo(e){
         setNewTodo(e.target.value);
     }
     function addTodo(){
         setTodos([...todos,newTodo])
     }
+
     var deleteTodo = React.useCallback(function(ind){
         console.log("ind:",ind)
         console.log("todos:",todos)
@@ -19,11 +22,12 @@ function Todolist() {
     },[todos])
     useEffect(()=>{
         console.log("Todolist useEffect with empty dep array")
+        nref.current.focus();
     },[])
     return (
         <div className='p-2 border border-2 border-success'>
             <h2>Todolist</h2>
-            <input type="text" onKeyUp={(e)=>{updateNewTodo(e)}}/>
+            <input type="text" onKeyUp={(e)=>{updateNewTodo(e)}} ref={nref}/>
             <button onClick={()=>{addTodo()}}>Add Todo</button>
             {
                 todos.map((todo,i)=>{
